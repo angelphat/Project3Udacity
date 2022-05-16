b5dc10b634229cdd2f6dc0326b71696c7c5cf9c8
PhatKT <>
2022-05-16 11:10:33 +0700

    no message

--- udagram-api-user/env.sh
+++ udagram-api-user/env.sh
@@ -7,4 +7,6 @@ export AWS_REGION=us-east-1a
 export AWS_PROFILE=phatkt
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+export DOCKER_USERNAME=phatkhatuan
+export DOCKER_PASSWORD=2c3v@2023
 
